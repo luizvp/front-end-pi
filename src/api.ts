37f6a204d65fd1c7ac4974908dd538e1cbe5092e
Laharl <fizzdosks@gmail.com>
2025-04-05 11:32:44 -0300

    .

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from 'axios';
 
 // Create an Axios instance with a base URL
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://api.luizvieira.xyz/api/',  // Replace with your base URL
+  baseURL: 'https://api.luizvieira.xyz/api/',  // Replace with your base URL
 });
 
 export default api;
